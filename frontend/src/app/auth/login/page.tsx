c3cbfbb7d55fb4973e174decdacaebf441ddbef4
mhaddaou <medo19981602@gmail.com>
2024-10-22 15:14:14 +0100

    update: update dispying items

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,20 +1,10 @@
-"use client";
-
 import { SiginForm } from "@/app/components/main/SigninForm";
-import { useState } from "react";
 
 export default function Register() {
-  // const tasks = await prisma.task.findMany()
-  const [isSignup, setIsSignup] = useState(false);
-
   return (
-    <div className=" w-screen min-h-screen     overflow-x-hidden  bg-background py-12">
+    <div className=" w-screen min-h-screen     overflow-x-hidden bg-background flex justify-center items-center ">
       <div className="w-full h-full container ">
-        <div className="w-full flex items-center justify-center  h-full">
-          <div className="w-full  ">
-            <SiginForm />
-          </div>
-        </div>
+        <SiginForm />
       </div>
     </div>
   );
